fb960af2ed9eb5fa61715a9a6af4497e49ce4ebb
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2024-01-12 12:16:44 -0600

    feat: FillBytes and ReadBytes

--- stream.go
+++ stream.go
@@ -2,6 +2,8 @@ package compress
 
 import (
 	"bytes"
+	"encoding/binary"
+	"errors"
 	"hash"
 	"math/big"
 
@@ -58,6 +60,8 @@ func (s *Stream) BreakUp(nbSymbs int) Stream {
 	return Stream{d, nbSymbs}
 }
 
+// todo @tabaie too many copy pastes in the next three funcs
+
 func (s *Stream) Pack(nbBits int) []*big.Int {
 	wordLen := bitLen(s.NbSymbs)
 	wordsPerElem := (nbBits - 1) / wordLen
@@ -77,6 +81,80 @@ func (s *Stream) Pack(nbBits int) []*big.Int {
 		}
 	}
 	return packed
+}
+
+func (s *Stream) FillBytes(bytes []byte, nbBits int) error {
+	bitsPerWord := bitLen(s.NbSymbs)
+	wordsPerElem := (nbBits - 1) / bitsPerWord
+	bytesPerElem := (nbBits + 7) / 8
+
+	if len(bytes) < (len(s.D)*bitsPerWord+7)/8+4 {
+		return errors.New("not enough room in bytes")
+	}
+
+	binary.BigEndian.PutUint32(bytes[:4], uint32(len(s.D)))
+	bytes = bytes[4:]
+
+	var radix, elem big.Int // todo @tabaie all this big.Int business seems unnecessary. try using bitio instead?
+	radix.Lsh(big.NewInt(1), uint(bitsPerWord))
+
+	for i := 0; i < len(bytes); i += bytesPerElem {
+		elem.SetInt64(0)
+		for j := wordsPerElem - 1; j >= 0; j-- {
+			absJ := i*wordsPerElem + j
+			if absJ >= len(s.D) {
+				continue
+			}
+			elem.Mul(&elem, &radix).Add(&elem, big.NewInt(int64(s.D[absJ])))
+		}
+		elem.FillBytes(bytes[i : i+bytesPerElem])
+	}
+	return nil
+}
+
+func (s *Stream) ReadBytes(byts []byte, nbBits int) error {
+	bitsPerWord := bitLen(s.NbSymbs)
+
+	if s.NbSymbs != 1<<bitsPerWord {
+		return errors.New("only powers of 2 currently supported for NbSymbs")
+	}
+
+	s.resize(int(binary.BigEndian.Uint32(byts[:4])))
+	byts = byts[4:]
+
+	wordsPerElem := (nbBits - 1) / bitsPerWord
+	bytesPerElem := (nbBits + 7) / 8
+	nbElems := (len(s.D) + wordsPerElem - 1) / wordsPerElem
+
+	if len(byts) < nbElems*bytesPerElem {
+		return errors.New("not enough bytes")
+	}
+
+	w := bitio.NewReader(bytes.NewReader(byts))
+
+	for i := 0; i < nbElems; i++ {
+		w.TryReadBits(uint8(8*bytesPerElem - bitsPerWord*wordsPerElem))
+		if i+1 == nbElems {
+			wordsToRead := len(s.D) - i*wordsPerElem
+			w.TryReadBits(uint8((wordsPerElem - wordsToRead) * bitsPerWord)) // skip unused bits
+		}
+		for j := 0; j < wordsPerElem; j++ {
+			wordI := i*wordsPerElem + j
+			if wordI >= len(s.D) {
+				break
+			}
+			s.D[wordI] = int(w.TryReadBits(uint8(bitsPerWord)))
+		}
+	}
+
+	return w.TryError
+}
+
+func (s *Stream) resize(_len int) {
+	if len(s.D) < _len {
+		s.D = make([]int, _len)
+	}
+	s.D = s.D[:_len]
 }
 
 func log(x, base int) int {
