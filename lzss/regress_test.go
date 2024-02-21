c60a83eea638e7f149d0bea5db86cd53cd48cea1
Gautam Botrel <gautam.botrel@gmail.com>
2024-02-21 10:33:34 -0600

    feat: add reference regression tests for compression ratio

---
+++ lzss/regress_test.go
@@ -0,0 +1,64 @@
+package lzss
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// For all the files in testdata/blobs/** we compress them and check the compression ratio against reference values
+
+type refValue struct {
+	lzssRatio float64
+}
+
+var refValues = map[string]refValue{
+	"./testdata/blobs/1-1865800": {
+		lzssRatio: 4.17,
+	},
+	"./testdata/blobs/1-goerli-3690632": {
+		lzssRatio: 24.23,
+	},
+	"./testdata/blobs/2-1865938": {
+		lzssRatio: 3.71,
+	},
+	"./testdata/blobs/3-1866069": {
+		lzssRatio: 3.53,
+	},
+	"./testdata/blobs/5-1128897": {
+		lzssRatio: 7.22,
+	},
+}
+
+func TestReferenceBlobs(t *testing.T) {
+	dict := getDictionary()
+	for filename, ref := range refValues {
+		t.Run(filename, func(t *testing.T) {
+			assert := require.New(t)
+			compressor, err := NewCompressor(dict, BestCompression)
+			assert.NoError(err)
+
+			// read filename
+			f, err := os.ReadFile(filename)
+			assert.NoError(err)
+
+			compressed, err := compressor.Compress(f)
+			assert.NoError(err)
+
+			// sanity check decompression matches
+			decompressed, err := Decompress(compressed, dict)
+			assert.NoError(err)
+			assert.Equal(f, decompressed)
+
+			// check compression ratio
+			lzssRatio := float64(len(f)) / float64(len(compressed))
+
+			t.Logf("%s: original size: %d, compressed size: %d, lzss ratio: %.2f", filename, len(f), len(compressed), lzssRatio)
+
+			assert.InDelta(ref.lzssRatio, lzssRatio, 0.05)
+
+		})
+	}
+
+}
